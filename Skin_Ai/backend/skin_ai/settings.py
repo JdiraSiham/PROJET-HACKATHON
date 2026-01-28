0689fe0a670cde64015a8d34a2eb515e4f79a9f5
JdiraSiham <jdirasiham@gmail.com>
2026-01-28 19:43:52 +0100

    Update settings.py

--- Skin_Ai/backend/skin_ai/settings.py
+++ Skin_Ai/backend/skin_ai/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(5z%^+hh@)hmcbn2+grb%c*7*)kxi#_t+iqz7qnk-q6rh$4h)a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*', "*.koyeb.app"]
 
 
 # Application definition
